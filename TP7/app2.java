31874887741fcdc0504113bae1a50f8e77e7dc8b
unknown <selmenawididi@gmail.com>
2025-02-27 00:24:21 +0100

    je corrige des fautes

--- TP7/app2.java
+++ TP7/app2.java
@@ -3,50 +3,42 @@ import java.util.Scanner;
 public class app2 {
 
     public static void main(String[] args) {
-        try(Scanner scanner=new Scanner(System.in)){
-        int nl,nc,Moy,i,j;
-        int P=1;
-        int S=0;
-        int M=new int[30][30];
-        do{
-            System.out.println("donner nl");
-            nl=scanner.nextLine();
-            System.out.println("donner nc");
-            nc=scanner.nextLine();
-        }while(nl>=30 || nc>=30);
-        for(i=0;i<=nl;i++){
-            for(j=0;j<=nc;j++){
-                System.out.println("M["+i+","+j+"]");
-                M[i][j]=scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            int nl, nc, Moy, i, j;
+            int P = 1;
+            int S = 0;
+            int[][] M = new int[30][30];
 
+            
+            do {
+                System.out.println("Donner nl (nl < 30): ");
+                nl = scanner.nextInt();
+                System.out.println("Donner nc (nc < 30): ");
+                nc = scanner.nextInt();
+            } while (nl >= 30 || nc >= 30);
+
+            
+            for (i = 0; i < nl; i++) {
+                for (j = 0; j < nc; j++) {
+                    System.out.println("M[" + i + "," + j + "]: ");
+                    M[i][j] = scanner.nextInt();
+                }
             }
+
+           
+            for (i = 0; i < nl; i++) {
+                for (j = 0; j < nc; j++) {
+                    P *= M[i][j];
+                    S += M[i][j];
+                }
+            }
+
+            
+            Moy = S / (nl * nc);
+
+            // Output results
+            System.out.println("Produit = " + P);
+            System.out.println("Moyenne = " + Moy);
         }
-        for(i=0;i<=nl;i++){
-            for(j=0;j<=nc;j++){
-                P=M[i][j]*P;
-                S=S+M[i][j];
-                Moy=(i+j)/S;
-            
-            
-            
-            
-            
-            
-            
-            
-            }}
-            System.out.println("produit= "+P);
-            System.out.println("moyenne est = "+Moy);
-
-
-
-        
-        
-        
-        
-        
-
-
     }
-}
 }
