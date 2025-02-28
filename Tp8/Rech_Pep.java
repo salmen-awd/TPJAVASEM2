24149d9eacec38fe989d02da6b3a26e0a34c410c
unknown <selmenawididi@gmail.com>
2025-02-28 10:45:24 +0100

    correction des fautes

--- Tp8/Rech_Pep.java
+++ Tp8/Rech_Pep.java
@@ -1,38 +1,21 @@
 import java.util.Scanner;
 
-
-class  Rech_Pep{
+class Rech_Pep {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            String mot1, mot2; 
-            StringBuffer text;
-            int L,P;
-            System.out.println("saisir un text");
-            text=new StringBuffer(scanner.nextLine());
-            System.out.println("Donner un mot 1");
-            mot1=scanner.next();
-            System.out.println("Donner un mot2");
-            mot2=scanner.next();
-            if (text.indexOf(mot1) == -1) {
-                System.out.println(mot1+" "+"n existe pas dans"+" "+text);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("saisir un text");
+        String text = scanner.nextLine();
+        System.out.println("Donner un mot 1");
+        String mot1 = scanner.next();
+        System.out.println("Donner un mot 2");
+        String mot2 = scanner.next();
 
-                
-                
-            } else {
-                L=mot1.length();
-            while (text.indexOf(mot1) != -1) {
-                P = text.indexOf(mot1);
-                text.delete(P, L);
-                text.insert(P, mot2);
-            }
+        if (text.indexOf(mot1)==-1) {
+            System.out.println(mot1 + " n'existe pas dans " + text);
+        } else {
+            text = text.replace(mot1, mot2);
         }
-        System.out.println("le nouveau text est :"+text);
 
-    }
-
-    
-    
-
-
+        System.out.println("le nouveau text est : " + text);
     }
 }
